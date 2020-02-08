089f7f203497a871a95dcb26b6ff56d5f129bfdd
dash2927 <dash2927@uw.edu>
2020-02-08 03:08:44 +0000

    Changed anim_plot.py to be more functional

--- modules/anim_plot.py
+++ modules/anim_plot.py
@@ -20,41 +20,61 @@ def enablePrint():
     """
     sys.stdout = sys.__stdout__
     
-def rotate_3d(df, x, y, z, save_file=True):
+def rotate_3d(df, feat, save_file=True, **kwargs):
     """
     Creates a .gif file of the rotating 3d plot with gives axes
     
     Parameters
     ----------
-    df : pd.dataFrame : 
-        dataframe of the results data taken from the prediciton study. Assumes
-        results are in df['Predicted'] and there are 4 classes to predict
-    x : string :
-        x axis df label for 3d plot
-    y : string : 
-        y axis df label for 3d plot
-    z : string :
-        z axis df label for 3d plot
+    df        : pd.dataFrame : 
+              dataframe of the results data taken from the prediciton study. Assumes
+              results are in df['Predicted'] and there are 4 classes to predict
+    feat      : [string] :
+              [x, y, z] axis df label for 3d plot
+    save_file : bool :
+              save file or not
+    
+    kwargs include override parameter dicts for matplotlib.animation.FuncAnimation()
+    and matplotlib.animation.FuncAnimation.save(). See API for both of these. 
+    Label is 'anim_param' and 'save_param' respectively.
+    
     Returns
     -------
     rot_animation : object :
         FuncAnimation object containing animation data
     """
     
+    assert all([(i in df.columns) for i in feat]), "Feature(s) not in the dataframe"
+    assert len(feat) == 3, "Inputed too many features"
+    
+    pred_list = df.predicted.unique()
+    # Animation/Saving parameters:
+    anim_param = {'frames':np.arange(0,407,2),
+                  'interval':50,
+                  'repeat':True
+                 }
+    save_param = {'filename':f'./gif_rotation_{feat[0]}_{feat[1]}_{feat[2]}.gif'.replace(' ', ''),
+                  'writer':'imagemagick',
+                  'fps':20
+                 }
+    # Override for making and saving animations
+    if 'anim_param' in kwargs:
+        anim_param.update(kwargs['anim_param'])
+    if 'save_param' in kwargs:
+        save_param.update(kwargs['save_param'])
+        
+    # Initial conditions
     def init():
-        ax.scatter(df_P14_pred[x], df_P14_pred[y], df_P14_pred[z], marker = 'o', alpha = 0.2)
-        ax.scatter(df_P21_pred[x], df_P21_pred[y], df_P21_pred[z], marker = 'o', alpha = 0.2)
-        ax.scatter(df_P28_pred[x], df_P28_pred[y], df_P28_pred[z], marker = 'o', alpha = 0.2)
-        ax.scatter(df_P35_pred[x], df_P35_pred[y], df_P35_pred[z], marker = 'o', alpha = 0.2)
-
-        ax.set_xlabel(x)
-        ax.set_ylabel(y)
-        ax.set_zlabel(z)
-        leg = pl.legend(['P14', 'P21', 'P28', 'P35'])
+        for cat in pred_list:
+            ax.scatter(dict_list[cat][feat[0]], dict_list[cat][feat[1]], dict_list[cat][feat[2]], marker = 'o', alpha = 0.2)
+        ax.set_xlabel(feat[0])
+        ax.set_ylabel(feat[1])
+        ax.set_zlabel(feat[2])
+        leg = pl.legend(pred_list)
         for lh in leg.legendHandles: 
             lh.set_alpha(1)
         return fig,
-
+    # Changing conditions
     def rotate(angle):
         ax.view_init(azim=angle)
         return fig,
@@ -64,17 +84,14 @@ def rotate_3d(df, x, y, z, save_file=True):
     # Disable warnings that FuncAnimation() gives
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        
         fig = pl.figure()
         ax = Axes3D(fig)
-        df_P14_pred = df[df['predicted'] == 0]
-        df_P21_pred = df[df['predicted'] == 1]
-        df_P28_pred = df[df['predicted'] == 2]
-        df_P35_pred = df[df['predicted'] == 3]
-
-        rot_animation = animation.FuncAnimation(fig, rotate, init_func=init, frames=np.arange(0,407,2),interval=50, repeat=True)
+        dict_list = []
+        for tag in pred_list:
+            dict_list.append(df[df['predicted']==tag])
+        rot_animation = animation.FuncAnimation(fig=fig, func=rotate, init_func=init, **anim_param)
         if save_file:
-            rot_animation.save(f'./gif_rotation_{x}_{y}_{z}.gif'.replace(' ', ''), writer='imagemagick', fps=20)
+            rot_animation.save(**save_param)
         # Close plot if using %matplotlib inline
         pl.close()
     return rot_animation
